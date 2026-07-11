8b9f25d12d757feac97e02306d90a2f8b5682bcc
Sulaiman Ibrahim Kayarda <sulaimankayarda56@gmail.com>
2026-07-11 12:20:58 +0100

    Update Railway startup logging

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://maizeexpensesystem-production.up.railway.app/api"
 });
 
 // Automatically attach JWT token to every request
